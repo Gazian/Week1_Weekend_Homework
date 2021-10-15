24e45afc375f5dca5cdc4babba322e10bd51949c
Jean-Pierre Gazian <jeanpierregazian@msn.com>
2021-10-16 00:08:34 +0100

    Committing second answer - get_total_cash(self)

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -4,4 +4,6 @@
 def get_pet_shop_name(cc_pet_shop):
     return cc_pet_shop["name"]
 
-
+#Function 2 - def test_total_cash(self) input
+def get_total_cash(cc_pet_shop):
+    return cc_pet_shop["admin"]["total_cash"]
