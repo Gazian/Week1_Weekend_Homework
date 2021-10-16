bc5831e30509a58ded8207ae3540af97e83a5dfc
Jean-Pierre Gazian <jeanpierregazian@msn.com>
2021-10-16 22:02:56 +0100

    Answers 3&4 - Add & Remove Cash functions

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,9 +1,18 @@
 # WRITE YOUR FUNCTIONS HERE
 
-#Function 1 - def test_pet_shop_name(self) input
+#Function 1 - def test_pet_shop_name(self)
 def get_pet_shop_name(cc_pet_shop):
     return cc_pet_shop["name"]
 
-#Function 2 - def test_total_cash(self) input
+#Function 2 - def test_total_cash(self)
 def get_total_cash(cc_pet_shop):
     return cc_pet_shop["admin"]["total_cash"]
+
+#Function 3 - test_add_or_remove_cash__add & test_add_or_remove_cash__remove
+def add_or_remove_cash(cc_pet_shop,value):
+    sum = get_total_cash(cc_pet_shop) + value
+    cc_pet_shop["admin"]["total_cash"] =sum
+    cash = get_total_cash(cc_pet_shop)
+    return cash
+
+
