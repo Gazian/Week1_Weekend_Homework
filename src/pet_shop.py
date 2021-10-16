dcf43bc00a8e742a04e6a79c99f6e911b3742de1
Jean-Pierre Gazian <jeanpierregazian@msn.com>
2021-10-16 22:48:20 +0100

    Adding Function 6 - Input - def test_stock_count(self)

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -19,3 +19,16 @@ def add_or_remove_cash(cc_pet_shop,value):
 def get_pets_sold(cc_pet_shop):
     return cc_pet_shop["admin"]["pets_sold"]
 
+#Function 5 - Input - test_increase_pets_sold(self)
+def increase_pets_sold(cc_pet_shop,value):
+    sum = get_pets_sold(cc_pet_shop) + value
+    cc_pet_shop["admin"]["pets_sold"] =sum
+    sold = get_pets_sold(cc_pet_shop)
+    return sold
+
+#Function 6 - Input - def test_stock_count(self)
+def get_stock_count (cc_pet_shop):
+    stock = 0
+    for pet in cc_pet_shop["pets"]:
+        stock +=1
+    return stock
