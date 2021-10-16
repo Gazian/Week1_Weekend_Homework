c728222e00675507020ffa1b5a452d732034d111
Jean-Pierre Gazian <jeanpierregazian@msn.com>
2021-10-16 22:17:01 +0100

    Adding Function 4 - Input - def test_pets_sold(self)

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,18 +1,21 @@
 # WRITE YOUR FUNCTIONS HERE
 
-#Function 1 - def test_pet_shop_name(self)
+#Function 1 - Input - def test_pet_shop_name(self)
 def get_pet_shop_name(cc_pet_shop):
     return cc_pet_shop["name"]
 
-#Function 2 - def test_total_cash(self)
+#Function 2 - Input - def test_total_cash(self)
 def get_total_cash(cc_pet_shop):
     return cc_pet_shop["admin"]["total_cash"]
 
-#Function 3 - test_add_or_remove_cash__add & test_add_or_remove_cash__remove
+#Function 3 - Input - test_add_or_remove_cash__add & test_add_or_remove_cash__remove
 def add_or_remove_cash(cc_pet_shop,value):
     sum = get_total_cash(cc_pet_shop) + value
     cc_pet_shop["admin"]["total_cash"] =sum
     cash = get_total_cash(cc_pet_shop)
     return cash
 
+#Function 4 - Input - def test_pets_sold(self)
+def get_pets_sold(cc_pet_shop):
+    return cc_pet_shop["admin"]["pets_sold"]
 
